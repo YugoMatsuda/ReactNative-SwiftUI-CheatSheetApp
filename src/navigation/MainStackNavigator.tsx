8201ce1aefc0d5b76adc44d05404a3226e846f03
Yugo Matsuda <yugo1997zazen@gmail.com>
2025-03-11 08:39:57 -0700

    Setup Roots

---
+++ src/navigation/MainStackNavigator.tsx
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import RootScreen from '../screens/RootScreen';
+
+const Stack = createStackNavigator();
+
+function MainStackNavigator () {
+    return (
+        <Stack.Navigator>
+          <Stack.Screen name="SwiftUI CheatSheet App" component={RootScreen} />
+        </Stack.Navigator>
+      );
+}
+
+export default MainStackNavigator;
