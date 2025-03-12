829c088b7d2dcdb8920a7a0f668b2fc40eacbcdf
Yugo Matsuda <yugo1997zazen@gmail.com>
2025-03-12 14:26:49 -0700

    Add ButtonScreen

--- src/screens/ButtonScreen.tsx
+++ src/screens/ButtonScreen.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TouchableOpacity, Alert } from "react-native";
+import React from "react";
 
 const ButtonScreen = () => {
   return (
-    <View>
-      <Text>ButtonScreen</Text>
-    </View>
-  )
-}
+    <View className="flex-1 justify-center">
+      {/* If you don't wrap Button in View, the tap area will be full screen width. */}
+      <View className="self-center">
+        <Button
+          title="Button"
+          onPress={() => {
+            console.log("didTap Button");
+          }}
+        />
+      </View>
 
-export default ButtonScreen
+      <TouchableOpacity
+        className="bg-blue-500 px-4 py-2 self-center"
+        onPress={() => {
+          console.log("didTap Text Button");
+        }}
+      >
+        <Text className="text-white">Text Button</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ButtonScreen;
